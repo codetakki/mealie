706424dc018a7b2b8fe2a9a7a017982987f75ba6
Tarek Auf der Strasse <69731049+codetakki@users.noreply.github.com>
2024-11-09 17:35:17 +0100

    Changed margins to not cut drop shadow

--- frontend/components/Domain/Recipe/RecipePage/RecipePage.vue
+++ frontend/components/Domain/Recipe/RecipePage/RecipePage.vue
@@ -93,13 +93,13 @@
       </v-row>
 
     </v-sheet>
-    <v-sheet v-show="isCookMode && hasLinkedIngredients" class="px-2 px-md-4" >
-      <div class="mt-2">
+    <v-sheet v-show="isCookMode && hasLinkedIngredients">
+      <div class="mt-2 px-2 px-md-4">
         <RecipePageScale :recipe="recipe" :scale.sync="scale" :landscape="landscape"/>
       </div>
       <RecipePageInstructions
         v-model="recipe.recipeInstructions"
-        class="overflow-y-hidden mt-n5"
+        class="overflow-y-hidden mt-n5 px-2 px-md-4"
         :assets.sync="recipe.assets"
         :recipe="recipe"
         :scale="scale"
